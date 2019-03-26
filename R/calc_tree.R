9c71eaa822c11d76ceca31a550902dca5914a745
hkaspersen <hakon.kaspersen@protonmail.com>
2019-03-26 11:13:14 +0100

    changed calc_dist to calc_tree

---
+++ R/calc_tree.R
@@ -0,0 +1,19 @@
+# calculate distances and generate basic
+# dendrogram from chewBBACA cgMLST data
+calc_tree <- function(path,
+                      metric = "gower",
+                      method = "average") {
+  # read cgMLST data
+  data <- read.table(path,
+                     sep = "\t",
+                     header = TRUE,
+                     colClasses = "factor") %>%
+    na_if("0") %>%
+    column_to_rownames("FILE")
+
+  # calculate distances and generate dendrogram
+  tree <- as.phylo(hclust(daisy(data,
+                                metric = metric),
+                          method = method))
+  return(tree)
+}
