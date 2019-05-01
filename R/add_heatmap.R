fff99abd678b96a3bcf6b2ab9a68a918f5b7679d
Kaspersen <vi1511@vetinst.no>
2019-05-01 22:01:59 +0200

    added functionality for layout

--- R/add_heatmap.R
+++ R/add_heatmap.R
@@ -4,12 +4,14 @@
 #'
 #' @param tree A tree object to annotate
 #' @param heatmap_data A file containing the data to add heatmap of. The first column need to hold the same ID's used to create the tree object. The remaining columns will be used in the heatmap. Each unique value in the whole file, except the ID column, will be given a unique color
+#' @param layout The layout of the tree, either "circular", "rectangular", or other, (see \code{\link[ggtree]{ggtree}})
 #' @param perc_open How many percent the tree is opened. This is used to fit the heatmap labels in the gap produced
 #' @param perc_rotate How many percent the tree is rotated. This is to get the heatmap labels on the top pf the tree
 #' @param heatmap_offset The distance between the tree and the heatmap
 #' @param heatmap_width The width of the heatmap
 #' @param colnames_offset The distance between the heatmap column names and the heatmap
 #' @param colnames_position The location of the heatmap column names
+#' @param colnames_angle The angle of the column names
 #' @param font_size Font size of the heatmap column names
 #' @param palette_type Specify which colorBrewer palette to use. Choose from "Accent", "Dark2", "Paired", "Pastel1", "Pastel2", "Set1", "Set2", "Set3" (see \code{\link[RColorBrewer]{brewer.pal}})
 #' @param own_palette If a named vector of colors are specified here, they are used instead of the automatically generated colors from colorBrewer
@@ -27,12 +29,14 @@
 #'
 add_heatmap <- function(tree,
                         heatmap_data,
+                        layout = "circular",
                         perc_open = 10,
                         perc_rotate = 95,
                         heatmap_offset = 0.05,
                         heatmap_width = 0.5,
                         colnames_offset = 0,
                         colnames_position = "top",
+                        colnames_angle = 0,
                         font_size = 5,
                         palette_type = "Paired",
                         own_palette = NULL) {
@@ -56,22 +60,39 @@ add_heatmap <- function(tree,
     names(palette) <- vars
   }
 
-  # Open tree
-  open_tree <- rotate_tree(open_tree(tree,
-                                     perc_open),
-                           perc_rotate)
+  if (layout == "circular") {
+    # Open tree
+    open_tree <- rotate_tree(open_tree(tree,
+                                       perc_open),
+                             perc_rotate)
 
-  # Add heatmap
-  annot_tree <- gheatmap(
-    open_tree,
-    heatmap_df,
-    offset = heatmap_offset,
-    width = heatmap_width,
-    colnames_offset_y = colnames_offset,
-    colnames_position = colnames_position,
-    font.size = font_size
-  ) +
-    scale_fill_manual(values = palette)
+    # Add heatmap
+    annot_tree <- gheatmap(
+      open_tree,
+      heatmap_df,
+      offset = heatmap_offset,
+      width = heatmap_width,
+      colnames_offset_y = colnames_offset,
+      colnames_position = colnames_position,
+      colnames_angle = colnames_angle,
+      font.size = font_size
+    ) +
+      scale_fill_manual(values = palette)
+  }
+
+  if (layout != "circular") {
+    annot_tree <- gheatmap(
+      tree,
+      heatmap_df,
+      offset = heatmap_offset,
+      width = heatmap_width,
+      colnames_offset_y = colnames_offset,
+      colnames_position = colnames_position,
+      colnames_angle = colnames_angle,
+      font.size = font_size
+    ) +
+      scale_fill_manual(values = palette)
+  }
 
   return(annot_tree)
 }
