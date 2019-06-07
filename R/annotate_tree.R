03430e40ec2e4a6f233a75a5865b8b8850a4ba1c
Kaspersen <vi1511@vetinst.no>
2019-06-07 11:39:45 +0200

    added functionality for more labels and node labels

--- R/annotate_tree.R
+++ R/annotate_tree.R
@@ -12,6 +12,12 @@
 #' @param label_variable A string representing the column name in the metadata file to plot as labels on the tree
 #' @param label_size The font size of the labels
 #' @param label_offset The distance between the labels and the tree
+#' @param clade_label_node The node where the clade label is placed
+#' @param clade_label The label that will be placed on the clade
+#' @param cladelabel_offset The distance between the tree and the clade label
+#' @param align_cladelabel Should the clade label be aligned?
+#' @param align Should the labels align?
+#' @param node_labels Should node labels be included?
 #' @param palette_type Specify which colorBrewer palette to use. Choose from "Accent", "Dark2", "Paired", "Pastel1", "Pastel2", "Set1", "Set2", "Set3" (see \code{\link[RColorBrewer]{brewer.pal}})
 #' @param legend_position Specify legend position in plot
 #' @param own_palette If a named vector of colors are specified here, they are used instead of the automatically generated colors from colorBrewer
@@ -35,6 +41,12 @@ annotate_tree <- function(tree,
                           label_variable = NULL,
                           label_size = 4,
                           label_offset = 0.01,
+                          clade_label_node = NULL,
+                          clade_label = NULL,
+                          cladelabel_offset = NULL,
+                          align_cladelabel = FALSE,
+                          align = FALSE,
+                          node_labels = NULL,
                           palette_type = "Paired",
                           legend_position = "right",
                           own_palette = NULL) {
@@ -71,10 +83,18 @@ annotate_tree <- function(tree,
                     layout = layout,
                     color = tree_color,
                     size = line_width) %<+% metadata_df +
+          {if (!is.null(node_labels))
+            geom_text(aes(label=node))} +
+          {if (!is.null(clade_label))
+            geom_cladelabel(clade_label_node,
+                            clade_label,
+                            offset = cladelabel_offset,
+                            align = align_cladelabel)} +
           {if (!is.null(label_variable))
             geom_tiplab2(aes(label = !! sym(label_variable)),
                          offset = label_offset,
-                         size = label_size)} +
+                         size = label_size,
+                         align = align)} +
           {if (!is.null(color_variable))
             geom_tippoint(aes(color = !! sym(color_variable)),
                         size = tippoint_size)} +
@@ -86,10 +106,18 @@ annotate_tree <- function(tree,
                     layout = layout,
                     color = tree_color,
                     size = line_width) %<+% metadata_df +
+          {if (!is.null(node_labels))
+            geom_text(aes(label=node))} +
+          {if (!is.null(clade_label))
+            geom_cladelabel(clade_label_node,
+                            clade_label,
+                            offset = cladelabel_offset,
+                            align = align_cladelabel)} +
           {if (!is.null(label_variable))
             geom_tiplab(aes(label = !! sym(label_variable)),
                         offset = label_offset,
-                        size = label_size)} +
+                        size = label_size,
+                        align = align)} +
           {if (!is.null(color_variable))
             geom_tippoint(aes(color = !! sym(color_variable)),
                           size = tippoint_size)} +
